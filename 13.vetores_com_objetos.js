adfb443863b5fb50034b357bab0bb8e017a61503
aluno senai <eduardatxr.dev@gmail.com>
2026-02-27 20:55:14 -0300

    objetos

--- 13.vetores_com_objetos.js
+++ 13.vetores_com_objetos.js
@@ -1,24 +1,34 @@
 // Vetor de objetos
 
-const usuarios = [
-    {nome:"Ana", idade:25},
-    {nome:"Bruno", idade:22},
-    {nome:"carla", idade:30},
-    {nome:"Daniela", idade:15},
+const listaDeUsuarios = [
+    {nome:"Ana", idade: 25},
+    {nome:"Bruno", idade: 22},
+    {nome:"Carla", idade: 30},
+    {nome:"Daniela", idade: 15},
 
 ]
 //EXIBINDO TODOS OS ELEMENTOS DO VETOR
 console.log ("Exibindo todos os usuários de vetor: ")
-usuarios.forEach (usuario => {
-    console.log(`${usuario.nome}tem ${usuario.idade} anos.`)
+listaDeUsuarios.forEach (usuario => {
+    console.log(`${usuario.nome} tem ${usuario.idade} anos.`)
 })
 
-console.log("\n Filtrando idade maior que 18 anos: ")
+console.log("\nFiltrando idade maior que 18 anos: ")
 //ADICIONANDO EM NOVA LISTA APENAS MAIORES DE 18 ANOS.
-const maioresDe18 = usuarios.filter(
+const maioresDe18 = listaDeUsuarios.filter(
     usuario => usuario.idade >18
 )
 //EXIBINDO TODOS OS ELEMENTOS DO VETOR.
 maioresDe18.forEach(usuario => 
-    console.log(`${usuario.nome} tem${usuario.idade}`)
+    console.log(`${usuario.nome} tem ${usuario.idade}`)
 )
+
+console.log("\nMostrando apenas nomes:")
+const listaDeNomes = listaDeUsuarios.map (u=> u.nome)
+listaDeNomes.forEach(nome => console.log(`${nome}`))
+
+console.log("\nEncontrar um usuário: ")
+const usuarioEncontrado = listaDeUsuarios.find(u => u.nome === "Carla")
+
+console.log(usuarioEncontrado)
+console.log(`Nome: ${usuarioEncontrado.nome} \nIdade: ${usuarioEncontrado.idade}`)
